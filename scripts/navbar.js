1d45e162b304795a7ccd77676891f0f5c39d4e35
JinchuLi2002 <63861808+JinchuLi2002@users.noreply.github.com>
2020-09-01 21:50:23 -0700

    added responsive nav

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
   
     $("button").click(function(){
 
-      if($("button").text() == "☰"){
-        $("button").text("🞬");
-      }else{
+      if($("button").text() == "☰")
+      {
+        $("button").text("✕");
+        $("li").toggle("slow");
+        $("nav ul").css("flex-direction", "column");
+        $("nav li").css("display", "block");
+        $("nav li").css("text-align", "center");
+      }
+      else
+      {
+        $("li").toggle("slow");
         $("button").text("☰");
       }
-      
-      $("li").toggle("slow");
+
     });  
 });
